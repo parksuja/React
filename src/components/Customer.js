7a3a70e6d42a42f014b68de1c7f0270d0805d013
parksuja <njoy4089@nate.com>
2021-09-13 21:41:56 +0900

    day 2(1)

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,4 +1,6 @@
 import React from "react"; //react 라이브러리를 불러옵니다.
+import { TableRow } from "@material-ui/core"; //material-ui를 이용해서 TableRow를 받아와서 사용할 수 있도록 합니다.
+import { TableCell } from "@material-ui/core"; //마찬가지입니다.
 
 class Customer extends React.Component {
   //React.Component 라이브러리이자 컴포넌트 상속받아서 하나의 컴포넌트를정의가능
@@ -6,46 +8,16 @@ class Customer extends React.Component {
     //항상 수행되는 내용 (return 함수 = 실제 그려지는 내용 작성 )
 
     return (
-      <div>
-        <CustomerProfile
-          id={this.props.id}
-          image={this.props.image}
-          name={this.props.name}
-        />
-        <CustomerInfo
-          birthday={this.props.birthday}
-          gender={this.props.gender}
-          job={this.props.job}
-        />
-      </div>
-    );
-  }
-}
-
-//사용자의 생년월일 , 성별 , 직업 렌더링하는 부분입니다.
-class CustomerInfo extends React.Component {
-  render() {
-    return (
-      <div>
-        <p>{this.props.birthday}</p>
-        <p>{this.props.gender}</p>
-        <p>{this.props.job}</p>
-      </div>
-    );
-  }
-}
-
-//사용자의 이름 아이디값 이미지를 렌더링하는 부분입니다.
-class CustomerProfile extends React.Component {
-  render() {
-    return (
-      <div>
-        <img src={this.props.image} alt="profile" />
-        <h2>
-          {this.props.name}
-          {this.props.id}
-        </h2>
-      </div>
+      <TableRow>
+        <TableCell>{this.props.id}</TableCell>
+        <TableCell>
+          <img src={this.props.image} alt="profile" />
+        </TableCell>
+        <TableCell>{this.props.name}</TableCell>
+        <TableCell>{this.props.birthday}</TableCell>
+        <TableCell>{this.props.gender}</TableCell>
+        <TableCell>{this.props.job}</TableCell>
+      </TableRow>
     );
   }
 }
