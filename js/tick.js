0d55ca83dfc7ad2afc680de25bff4e93d926a55d
parksuja <njoy4089@nate.com>
2021-09-27 20:20:29 +0900

    틱택토 수정 09-27

--- js/tick.js
+++ js/tick.js
@@ -3,8 +3,62 @@ var 테이블 = document.createElement("table");
 var 칸들 = [];
 var 줄들 = [];
 var 턴 = "X";
+var 다참;
+//컴퓨터가 클릭했을때 다찾을때 / 내가 클릭했을때 다찾을때 공통부분 함수로
+function 결과(몇줄, 몇칸) {
+  다참 = false;
+  // 가로줄 검사
+  if (
+    칸들[몇줄][0].textContent === 턴 &&
+    칸들[몇줄][1].textContent === 턴 &&
+    칸들[몇줄][2].textContent === 턴
+  ) {
+    다참 = true;
+  }
+  // 세로줄 검사
+  if (
+    칸들[0][몇칸].textContent === 턴 &&
+    칸들[1][몇칸].textContent === 턴 &&
+    칸들[2][몇칸].textContent === 턴
+  ) {
+    다참 = true;
+  }
+  // 대각선 검사가 필요한 경우 줄 - 칸이 0 , 절댓값으로 2가 되는 경우)
+  if (
+    칸들[0][0].textContent === 턴 &&
+    칸들[1][1].textContent === 턴 &&
+    칸들[2][2].textContent === 턴
+  ) {
+    다참 = true;
+  } else if (
+    칸들[0][2].textContent === 턴 &&
+    칸들[1][1].textContent === 턴 &&
+    칸들[2][0].textContent === 턴
+  ) {
+    다참 = true;
+  }
+}
+function 초기화(무승부) {
+  if (무승부) {
+    alert("무승부");
+  } else {
+    alert(턴 + "님이 승리하였습니다");
+  }
+  setTimeout(function () {
+    //모든값들을 빈칸으로 변경 초기화
+    칸들.forEach(function (줄) {
+      줄.forEach(function (칸) {
+        칸.textContent = "";
+      });
+    });
+    턴 = "X";
+  }, 1000);
+}
 
 var 비동기콜백 = function (e) {
+  if (턴 === "O") {
+    return;
+  }
   console.log(e.target);
   console.log(e.target.parentNode);
   console.log(줄들);
@@ -20,55 +74,49 @@ var 비동기콜백 = function (e) {
   } else {
     console.log("빈칸입니다.");
     칸들[몇줄][몇칸].textContent = 턴;
-    var 다참 = false;
-    // 가로줄 검사
-    if (
-      칸들[몇줄][0].textContent === 턴 &&
-      칸들[몇줄][1].textContent === 턴 &&
-      칸들[몇줄][2].textContent === 턴
-    ) {
-      다참 = true;
-    }
-    // 세로줄 검사
-    if (
-      칸들[0][몇칸].textContent === 턴 &&
-      칸들[1][몇칸].textContent === 턴 &&
-      칸들[2][몇칸].textContent === 턴
-    ) {
-      다참 = true;
-    }
-    // 대각선 검사가 필요한 경우 줄 - 칸이 0 , 절댓값으로 2가 되는 경우)
-    if (몇줄 - 몇칸 === 0 || Math.abs(몇줄 - 몇칸) === 2) {
-      if (
-        칸들[0][0].textContent === 턴 &&
-        칸들[1][1].textContent === 턴 &&
-        칸들[2][2].textContent === 턴
-      ) {
-        다참 = true;
-      } else if (
-        칸들[0][2].textContent === 턴 &&
-        칸들[1][1].textContent === 턴 &&
-        칸들[2][0].textContent === 턴
-      ) {
-        다참 = true;
-      }
-    }
-    //다 찼으면
-    if (다참) {
-      alert(턴 + "님이 승리하였습니다");
-      턴 = "X";
-      //모든값들을 빈칸으로 변경 초기화
-      칸들.forEach(function (줄) {
-        줄.forEach(function (칸) {
-          칸.textContent = "";
-        });
+    결과(몇줄, 몇칸);
+    var 후보칸 = [];
+    칸들.forEach(function (줄) {
+      줄.forEach(function (칸) {
+        후보칸.push(칸);
       });
+    });
+    //다 찼으면
+    //빈 칸중 하나를 고른다
+    var 후보칸 = [];
+    칸들.forEach(function (줄) {
+      줄.forEach(function (칸) {
+        후보칸.push(칸);
+      });
+    });
+    후보칸 = 후보칸.filter(function (칸) {
+      return !칸.textContent;
+    });
+    if (다참) {
+      초기화();
+    } else if (후보칸.length === 0) {
+      //칸을 더 이상 선택할 수 없음
+      초기화(true);
     } else {
       if (턴 === "X") {
         턴 = "O";
-      } else {
-        턴 = "X";
       }
+      setTimeout(function () {
+        alert("컴퓨터의 턴입니다.");
+
+        var 선택칸 = 후보칸[Math.floor(Math.random() * 후보칸.length)];
+        선택칸.textContent = 턴;
+        //컴퓨터가 승리했는지 체크
+        var 몇줄 = 줄들.indexOf(선택칸.parentNode);
+        var 몇칸 = 칸들[몇줄].indexOf(선택칸);
+        결과(몇줄, 몇칸);
+        //다 찼으면
+        if (다참) {
+          초기화();
+        }
+        //턴을 나한테 넘긴다.
+        턴 = "X";
+      }, 1000);
     }
   }
 };
--- src/App.js
+++ src/App.js
@@ -1,89 +1,44 @@
-import React, { Component } from "react";
-import Customer from "./components/Customer"; //App.js 외부에 customer있기에 import해줍니다.
-import "./App.css";
-import { Table } from "@material-ui/core";
-import { TableBody } from "@material-ui/core";
-import { TableHead } from "@material-ui/core";
-import { TableCell } from "@material-ui/core";
-import { TableRow } from "@material-ui/core";
-import { withStyles } from "@material-ui/styles";
-import { Paper } from "@material-ui/core"; //컴포넌트 외부를 감싸기 위해서 사용
+import { useState, useRef } from "react";
 
-const styles = (theme) => ({
-  root: {
-    //루트 클래스 정의
-    width: "100%", //너비 100%
-    overflowX: "auto", //X축으로 오버플로우 적용
-  },
-  table: {
-    minWidth: 1080, //테이블 크기가 1080이상 출력되도록 화면크기가 줄어들어도 1080이상 출력 가로스크롤 적용
-  },
-});
+const App = () => {
+  const [word, setWord] = useState("박수진");
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+  const inputRef = useRef(null);
 
-//보내고자 하는 데이터 명시
-const customer = [
-  {
-    id: 1,
-    image: "https://placeimg.com/64/64/1", //랜덤으로 이미지 보여줍니다.
-    name: "박수잔1",
-    birthday: "970221",
-    gender: "여자",
-    job: "대학생",
-  },
-  {
-    id: 2,
-    image: "https://placeimg.com/64/64/2", //랜덤으로 이미지 보여줍니다.
-    name: "박수잔2",
-    birthday: "970222",
-    gender: "여자",
-    job: "대학생",
-  },
-  {
-    id: 3,
-    image: "https://placeimg.com/64/64/3", //랜덤으로 이미지 보여줍니다.
-    name: "박수잔3",
-    birthday: "970223",
-    gender: "남자",
-    job: "대학생",
-  },
-];
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (word[word.length - 1] === value[0]) {
+      setResult("딩동댕");
+      setWord(value);
+      setValue("");
+      inputRef.current.focus();
+    } else {
+      setResult("땡");
+      setValue("");
+      inputRef.current.focus();
+    }
+  };
 
-class App extends Component {
-  render() {
-    console.log(this.props);
-    const { classes } = this.props;
-    return (
-      <Paper className={classes.root}> 
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {customer.map((c) => {
-              return (
-                <Customer
-                  key={c.id}
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
-                />
-              );
-            })}
-          </TableBody>
-        </Table>
-      </Paper>
-    );
-  }
-}
+  const onChange = (e) => {
+    setValue(e.currentTarget.value);
+  };
 
-export default withStyles(styles)(App); //withStyles 적용한 형태로 내보냅니다.
+  return (
+    <div>
+      <div>{word}</div>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="wordInput">글자를 입력</label>
+        <input
+          id="wordInput"
+          ref={inputRef}
+          value={value}
+          onChange={onChange}
+        />
+        <button>클릭</button>
+      </form>
+      <div>{result}</div>
+    </div>
+  );
+};
+export default App;
